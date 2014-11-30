3c122c224438d16e3af8f4ab4b45932791bc9510
KTMarc <marc@AirMarc.local>
2014-11-30 20:22:04 +0100

    Initial Commit

---
+++ FOODit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FOODit
+//
+//  Created by Marc Humet on 30/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FOODit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FOODit
+//
+//  Created by Marc Humet on 30/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
