372666ec7ff9988237c954c26f9d7362a086bf04
KTMarc <marc@AirMarc.local>
2015-01-05 20:43:13 +0100

    Almost done

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// MZAppearance
+#define COCOAPODS_POD_AVAILABLE_MZAppearance
+#define COCOAPODS_VERSION_MAJOR_MZAppearance 1
+#define COCOAPODS_VERSION_MINOR_MZAppearance 1
+#define COCOAPODS_VERSION_PATCH_MZAppearance 3
+
+// MZFormSheetController
+#define COCOAPODS_POD_AVAILABLE_MZFormSheetController
+#define COCOAPODS_VERSION_MAJOR_MZFormSheetController 2
+#define COCOAPODS_VERSION_MINOR_MZFormSheetController 4
+#define COCOAPODS_VERSION_PATCH_MZFormSheetController 1
+
+// MZFormSheetController/Core
+#define COCOAPODS_POD_AVAILABLE_MZFormSheetController_Core
+#define COCOAPODS_VERSION_MAJOR_MZFormSheetController_Core 2
+#define COCOAPODS_VERSION_MINOR_MZFormSheetController_Core 4
+#define COCOAPODS_VERSION_PATCH_MZFormSheetController_Core 1
+
+// PocketSVG
+#define COCOAPODS_POD_AVAILABLE_PocketSVG
+#define COCOAPODS_VERSION_MAJOR_PocketSVG 0
+#define COCOAPODS_VERSION_MINOR_PocketSVG 6
+#define COCOAPODS_VERSION_PATCH_PocketSVG 0
+
